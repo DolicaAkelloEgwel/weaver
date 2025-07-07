8ffa607a284b2596e3e9b8d912ebf5834910de68
DolicaAkelloEgwel <d.egwel@gmail.com>
2025-07-07 12:11:40 +0100

    a bit of tidying

--- weaver.py
+++ weaver.py
@@ -1,19 +1,19 @@
 import cv2
 
 # read the image file
-img = cv2.imread('input.jpg', 2)
-
-bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
+img = cv2.imread('tube.jpg', 2)
 
 # converting to its binary form
-bw = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
 
-ones_and_zeros = bw[1] / 255
+# divide by 255 to get an array of 1s and 0s - 1 is black and 0 is white
+ones_and_zeros = bw_img / 255
 ones_and_zeros = 1 - ones_and_zeros
 
 # just checking
 print(ones_and_zeros.shape)
 
+# write it to a file
 with open("output.txt", "w") as f:
     for line in ones_and_zeros:
         line = [str(int(val)) for val in line]
