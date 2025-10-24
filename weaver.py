bea7924ab222375e357742bff38bb90e6c35c1f2
Akello (234.48:4) <43887608+DolicaAkelloEgwel@users.noreply.github.com>
2025-10-24 15:27:06 +0100

    Multiple weave types

--- weaver.py
+++ weaver.py
@@ -1,25 +1,32 @@
 import cv2
 
-# read the image file
-img = cv2.imread('input.jpg', 2)
+# read the image file + check the shape
+base_img = cv2.imread('Code 0.png', 2)
+print(base_img.shape)
 
-# converting to its binary form
-bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
+def convert_image_to_ones_and_zeroes(image):
+    bw = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+    ones_and_zeros = bw[1] / 255
+    ones_and_zeros = 1 - ones_and_zeros
+    return ones_and_zeros
 
-# divide by 255 to get an array of 1s and 0s - 1 is black and 0 is white
-ones_and_zeros = bw_img / 255
-ones_and_zeros = 1 - ones_and_zeros
+# things are flipped for the base image
+base_image = 1 - convert_image_to_ones_and_zeroes(base_img)
 
-# just checking
-print(ones_and_zeros.shape)
+# these are the codes the student used
+CODES = [2,3,5,6]
 
-# write it to a file
+for code in CODES:
+    code_image = cv2.imread(f'Code {code}.png', 2)
+    print(base_image[206][:5])
+    code_image = convert_image_to_ones_and_zeroes(code_image) * (code - 1)
+    base_image = base_image + code_image
+
 with open("output.txt", "w") as f:
-    for line in ones_and_zeros:
-        line = [str(int(val)) for val in line]
-        f.write("".join(line) + "\n")
+    for line in base_image:
+    line = [str(int(val)) for val in line]
+    f.write("".join(line) + "\n")
 
-# display the b&w image
-cv2.imshow("Binary", bw_img)
+cv2.imshow("Binary", base_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
